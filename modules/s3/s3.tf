8d22611464baa03d2fb996dfa2ad19c63a4cb30a
rishabhanand26 <33449790+rishabhanand26@users.noreply.github.com>
2020-02-20 06:44:46 +0530

    Update s3.tf

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIATMVC5TXSHVEAUO53"
-  secret_key = "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"
-}
+
 
 resource "aws_s3_bucket" "test" {
   bucket = var.bucket
